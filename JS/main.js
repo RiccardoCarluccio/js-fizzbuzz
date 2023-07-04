074cc25bcfbbb6c1593c3f02d183f97e240e9b52
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-04 16:55:07 +0200

    End

--- JS/main.js
+++ JS/main.js
@@ -5,10 +5,13 @@ for (let i = 1; i <= 100; i++) {
 
   if (i % 3 === 0 && i % 5 === 0) {   //fizz buzz
     singleCell.classList.add("single-cell", "mandy-red");
+    singleCell.innerHTML = 'FIZZ BUZZ';
   } else if (i % 3 === 0) {   //fizz
     singleCell.classList.add("single-cell", "caribbean-green");
+    singleCell.innerHTML = 'FIZZ';
   } else if (i % 5 === 0) {   //buzz
     singleCell.classList.add("single-cell", "light-mustard-yellow");
+    singleCell.innerHTML = 'BUZZ';
   } else {  //number
     singleCell.classList.add("single-cell", "nice-blue")
     singleCell.innerHTML = i.toString();
