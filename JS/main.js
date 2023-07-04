b21418efcfceef3e5f5f8687d05bb98e0befb945
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-04 16:16:03 +0200

    Colors added (via file CSS)

--- JS/main.js
+++ JS/main.js
@@ -4,12 +4,13 @@ for (let i = 0; i < 100; i++) {
   let singleCell = document.createElement("div");
 
   if (i % 3 === 0 && i % 5 === 0) {
-    singleCell.classList.add()
+    singleCell.classList.add("mandy-red");
   } else if (i % 3 === 0) {
-    //fizz
-  } else if (i % 5 === 0) {
-    //buzz
-  } else {
+    singleCell.classList.add("caribbean-green");
+  } else if (i % 5 === 0) {   //buzz
+    singleCell.classList.add("light-mustard-yellow");
+  } else {  //number
+    singleCell.classList.add("nice-blue")
     singleCell.innerHTML = i.toString();
   }
 
