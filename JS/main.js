7716d9a860bdad4d5f68fe7682e5f32f023778a1
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-07-04 16:46:50 +0200

    v0.9

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,16 @@
 const fizzBuzzContainer = document.querySelector(".fizzbuzz-container");
 
-for (let i = 0; i < 100; i++) {
+for (let i = 1; i <= 100; i++) {
   let singleCell = document.createElement("div");
 
   if (i % 3 === 0 && i % 5 === 0) {   //fizz buzz
-    singleCell.classList.add("mandy-red");
+    singleCell.classList.add("single-cell", "mandy-red");
   } else if (i % 3 === 0) {   //fizz
-    singleCell.classList.add("caribbean-green");
+    singleCell.classList.add("single-cell", "caribbean-green");
   } else if (i % 5 === 0) {   //buzz
-    singleCell.classList.add("light-mustard-yellow");
+    singleCell.classList.add("single-cell", "light-mustard-yellow");
   } else {  //number
-    singleCell.classList.add("nice-blue")
+    singleCell.classList.add("single-cell", "nice-blue")
     singleCell.innerHTML = i.toString();
   }
 
